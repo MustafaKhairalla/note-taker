6f69837bfecfb119ebbc8f627724d6e34e6d7802
mustafa khairalla <mukhairalla@gmail.com>
2020-03-08 13:43:58 -0600

    create routs for app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,24 @@
 let express = require("express");
+let path = require("path");
 
-let port = 3000;
+let PORT = 3000;
 
+let app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+app.get("/notes",(req, res) => {
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
+
+
+//start the server
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+  });
